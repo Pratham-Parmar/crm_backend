48ebc8c16a337c6714100cfed299a6fb07543395
Pratham <parmarpratham1@gmail.com>
2022-07-29 01:30:50 +0530

    first commit

--- crm/settings.py
+++ crm/settings.py
@@ -121,7 +121,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "*",
