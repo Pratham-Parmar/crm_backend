9d5c8f31ed759cde63590f1969302167acc28e01
Anjay Goel <anjay.goel@gmail.com>
2022-08-15 22:13:53 +0530

    Fix rates

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login', views.login_user, name='login'),
     path('search', views.search, name='search'),
     path('add', views.add, name='add'),
+    path('delete', views.delete_rate, name='delete'),
     path('ports', views.ports, name='ports'),
     path('addPort', views.add_port, name='add_port'),
     path('logout', views.logout_user, name='logout'),
